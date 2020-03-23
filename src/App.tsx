6be183200d51b2bdf30e6140439d2b65d2843ff8
dima <tynindima@gmail.com>
2020-03-23 16:21:55 +0200

    First test gh-pagest

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
+import { Header } from './components/Header';
+import './App.scss';
 
-import './App.css';
-
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
 
 const App = () => (
-  <div className="App">
-    <h1>Tabs with router</h1>
-  </div>
+  <>
+    <Header />
+  </>
 );
 
 export default App;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './header.scss';
+
+export const Header = () => {
+  return (
+    <header>
+      <h2 className="header2">this is header</h2>
+    </header>
+  );
+};
