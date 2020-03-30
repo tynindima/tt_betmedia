8c98872a69eb1dd533a1a816a349cd0c63cb9cf6
dima <tynindima@gmail.com>
2020-03-30 16:20:47 +0300

    Updates

---
+++ precache-manifest.cf55ba6cbea51064d1f4eb7175671494.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fd8c81083bd3dd376bf535fdbfcdc16",
+    "url": "./index.html"
+  },
+  {
+    "revision": "264918f3c1c5057ab6c9",
+    "url": "./static/css/main.155aff43.chunk.css"
+  },
+  {
+    "revision": "c62d33dfe985149fe393",
+    "url": "./static/js/2.d28b31d0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.d28b31d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "264918f3c1c5057ab6c9",
+    "url": "./static/js/main.3108fb70.chunk.js"
+  },
+  {
+    "revision": "419e72abb386215ec6d0",
+    "url": "./static/js/runtime-main.491c8e82.js"
+  },
+  {
+    "revision": "481c6ca76242766ee06d1c9b1b64c562",
+    "url": "./static/media/Line_8.481c6ca7.svg"
+  },
+  {
+    "revision": "e1645e2451f6494160f6b42d2bfae54e",
+    "url": "./static/media/Rectangle 51.e1645e24.svg"
+  },
+  {
+    "revision": "9ffa4774e73a0dfe187666af15a89d5e",
+    "url": "./static/media/Rectangle 54.9ffa4774.svg"
+  },
+  {
+    "revision": "105ae180f18cca9bf8b98cb6fd6df3c3",
+    "url": "./static/media/Rectangle 8.105ae180.svg"
+  },
+  {
+    "revision": "2969c024b1a676355262c7011274b47c",
+    "url": "./static/media/clean_design.2969c024.svg"
+  },
+  {
+    "revision": "cd5f3f2d61eb985ba1c8d3f1320257d5",
+    "url": "./static/media/macbook.cd5f3f2d.svg"
+  },
+  {
+    "revision": "bdfa703f23a7b08fc978ca63445f77dd",
+    "url": "./static/media/mobile.bdfa703f.png"
+  },
+  {
+    "revision": "f751d5fdbe4c30f473d5749e32afc888",
+    "url": "./static/media/retina_ready.f751d5fd.svg"
+  },
+  {
+    "revision": "5f21ae72f7cf7cec8a0f76943f925c89",
+    "url": "./static/media/secure_data.5f21ae72.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tt_betmedia/precache-manifest.a68ac73092c8f1062a2c72955658c892.js"
+  "./precache-manifest.cf55ba6cbea51064d1f4eb7175671494.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tt_betmedia/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
